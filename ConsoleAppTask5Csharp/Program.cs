8e88169db5f524d742f5414e126c925e9ad62425
ElWin95 <elvinma@code.edu.az>
2023-03-15 00:11:14 +0400

    'beseqeder'

--- ConsoleAppTask5Csharp/Program.cs
+++ ConsoleAppTask5Csharp/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace ConsoleAppTask5Csharp
+﻿using System;
+
+namespace ConsoleAppTask5Csharp
 {
     internal class Program
     {
@@ -8,20 +10,72 @@
             /* 1) 1-1000 qeder ededlerin cemi 7 bolunurmu?
              */
 
-            int sum = 0;
-            for (int i = 1; i <= 1000; i++)
-            {
-                sum += i;
-            }
-            Console.WriteLine(sum);
-            if (sum % 7 == 0)
-            {
-                Console.WriteLine("1-1000 qeder ededlerin cemi 7-e bolunur");
-            }
-            else
-            {
-                Console.WriteLine("1-1000 qeder ededlerin cemi 7-e bolunmur");
-            }
+            //int sum = 0;
+            //for (int i = 1; i <= 1000; i++)
+            //{
+            //    sum += i;
+            //}
+            //if (sum % 7 == 0)
+            //{
+            //    Console.WriteLine("1-1000 qeder ededlerin cemi 7-e bolunur");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("1-1000 qeder ededlerin cemi 7-e bolunmur");
+            //}
+            #endregion
+            #region Task2
+            /* 2) 1-1000 qeder ededlerin icerisinde hem 7-e, hemde 8-e bolunenlerin siyahisi
+             */
+
+            //for (int i = 1; i <= 1000; i++)
+            //{
+            //    if (i % 7 == 0 && i % 8 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+
+            //}
+            #endregion
+            #region Task3
+            /* 3) 1-1000 qeder ederlerin icerisinden  ele ededleri cap et ki, reqemleri cemi 3-e bolunsun
+             */
+            //for (int i = 1; i <= 1000; i++)
+            //{
+            //    if (i % 3 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+            #endregion
+            #region Task4
+            /* 4) 1-1000 qeder ederlerin icerisinden ele ededleri cap etki reqemleri cemi 3 e bolunsun ve sonuncu reqem 3 olmasin.
+             */
+            //for (int i = 1; i <= 1000; i++)
+            //{
+            //    if (i % 3 == 0 && i % 10 != 3)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+            #endregion
+            #region Task5
+            /* 5)  1-1000 qeder ededlerin icerisinden  ele ededleri cap etki : hem reqemleri cemi 5-e bolunsun, hem de ozu 5-e bolunsun
+             */
+            //int sum = 0;
+            //for (int i = 1; i <= 1000; i++)
+            //{
+            //    int num = i;
+            //    while (num > 0)
+            //    {
+            //        sum += (num % 10);
+            //        num /= 10;
+            //    }
+            //    if (i % 5 == 0 && sum % 5 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
             #endregion
 
 
