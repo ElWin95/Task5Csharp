3088ae37a11ec2a9e70f98b30a21bbeb6e791b87
ElWin95 <elvinma@code.edu.az>
2023-03-14 22:34:52 +0400

    'test'

---
+++ ConsoleAppTask5Csharp/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleAppTask5Csharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, numDigit;
+            string strNum;
+        L1:
+            Console.Write("6 reqemli ededi daxil edin: ");
+            strNum = Console.ReadLine();
+            if (!int.TryParse(strNum, out num))
+            {
+                goto L1;
+            }
+            numDigit = (int)Math.Log10(num) + 1;
+            if (numDigit > 8)
+            {
+                goto L1;
+            }
+        }
+    }
+}
